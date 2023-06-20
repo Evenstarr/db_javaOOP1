78a311c40ee2c0c77c84332bae2ed833ce5be0b0
Evenstar <alhe@yandex.ru>
2023-06-20 18:42:14 +0300

    Lesson4 initial

---
+++ src/main/java/lesson4/Main.java
@@ -0,0 +1,33 @@
+package lesson4;
+
+import java.time.LocalDate;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        User oneUser = new User("Юзер1", "Юзеров1", "Юзерович1", LocalDate.now());
+        User twoUser = new User("АЮзер2", "Юзеров2", "Юзерович2", LocalDate.now());
+        User threeUser = new User("Юзер3", "БЮзеров3", "Юзерович3", LocalDate.now());
+
+        TreeSet<User> userTreeSet = new TreeSet<>(new UserComparator<>());
+        userTreeSet.add(oneUser);
+        userTreeSet.add(twoUser);
+        userTreeSet.add(threeUser);
+
+        System.out.println(userTreeSet);
+
+        Teacher Ivanov = new Teacher(2, "Иван", "Иванов",
+                "Иванович", LocalDate.now());
+        Teacher Petrov = new Teacher(3, "Петр", "Петров",
+                "Петрович", LocalDate.now());
+        Teacher Sidorov = new Teacher(1, "Сидор", "Сидоров",
+                "Сидорович", LocalDate.now());
+
+        TreeSet<Teacher> teacherTreeSet = new TreeSet<>(new TeacherComparator<>());
+        teacherTreeSet.add(Ivanov);
+        teacherTreeSet.add(Petrov);
+        teacherTreeSet.add(Sidorov);
+
+        System.out.println(teacherTreeSet);
+    }
+}
